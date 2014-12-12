d6f1ac4de637fe6219ed8de931813dd88eaa48da
Phil Cable <pcable@wsu.edu>
2014-12-12 07:29:46 -0800

    Adding HD Newsletter template, round 1

---
+++ theme-css/hd.php
@@ -0,0 +1,26 @@
+@media only screen and (max-width:	480px) {
+  body, table, td, p, a, li, blockquote {
+    -webkit-text-size-adjust: none !important;
+  }
+  body {
+    width: 100% !important;
+    min-width: 100% !important;
+  }
+  img {
+    height: auto;
+    width: 100% !important;
+  }
+  .full-width {
+    width: 100% !important;
+  }
+  .container {
+    padding: 0 !important;
+  }
+  .column {
+    display: block !important;
+    width: auto !important;
+  }
+  .share {
+    padding-left: 12px !important;
+  }
+}
