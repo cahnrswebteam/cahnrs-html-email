c0a455e3a6e3c75ef45279a48d4266052cb480e2
Phil Cable <pcable@wsu.edu>
2014-12-16 09:02:58 -0800

    HD template and styles

--- theme-css/hd.php
+++ theme-css/hd.php
@@ -1,4 +1,78 @@
-@media only screen and (max-width:	480px) {
+h1 {
+	color: #5e6a71;
+  margin: 0 0 12px;
+}
+p {
+	font-size: 14px;
+}
+.column-1 {
+  padding: 10px 15px 15px 15px;
+  text-align: left;
+  vertical-align: top;
+  width: 516px;
+}
+.column-1 h2 {
+	color: #981e32;
+  margin: 0 0 12px;
+}
+.column-2 {
+	background: #d7dadb;
+  border-bottom: 2px solid #ffffff;
+  text-align: left;
+  vertical-align: top;
+  width: 154px;
+}
+.column-2 table {
+	width: 154px;
+}
+.column-2 h2 {
+  color: #000;
+  font-size: 16px;
+  font-weight: normal;
+  margin: 0 0 12px;
+  text-transform: uppercase;
+}
+.column-2 p,
+.column-2 a {
+	color: #fff;
+  text-decoration: none;
+}
+.column-2 a:hover {
+	color: #000;
+}
+.column-2 table:first-of-type {
+	background: #5e6a71;
+  border-bottom: 2px solid #ffffff;
+}
+.column-2 table:nth-of-type(2) {
+	background: #8d959a;
+  border-bottom: 2px solid #ffffff;
+}
+.column-2 table:nth-of-type(3) {
+	background: #b5babe;
+  border-bottom: 2px solid #ffffff;
+}
+.column-2 table td {
+  padding: 12px 12px 20px 12px;
+  width: 131px;
+}
+.column-2 table:nth-of-type(4) p,
+.column-2 table:nth-of-type(4) a {
+	color: #5e6a71;
+  text-decoration: none;
+}
+.column-2 table:nth-of-type(4) a:hover {
+	color: #000;
+}
+.column-2 table:nth-of-type(4) strong {
+	color: #981e32;
+}
+
+table#row-200 {
+	display: none;
+}
+
+@media only screen and (max-width: 480px) {
   body, table, td, p, a, li, blockquote {
     -webkit-text-size-adjust: none !important;
   }
@@ -10,8 +84,12 @@
     height: auto;
     width: 100% !important;
   }
-  .full-width {
-    width: 100% !important;
+  .full-width,
+  .column-2 table {
+  	width: 100% !important;
+	}
+  .column-2 table td {
+    width: auto !important;
   }
   .container {
     padding: 0 !important;
--- theme-templates/hd.php
+++ theme-templates/hd.php
@@ -3,70 +3,56 @@
     <table align="center" bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="700" class="full-width" style="border-collapse:collapse; width:700px;">
       <tr>
         <td width="700" class="full-width" style="color:#981e32; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; font-size:20px; width:700px;">
-          <img src="<?php bloginfo('stylesheet_directory'); ?>/theme-images/hd-newsletter-header.gif" alt="Human Development Update" width="700" height="182" style="display:block;" />
+          <img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/hd-newsletter-header.gif" alt="Human Development Update" width="700" height="182" style="display:block;" />
         </td>
       </tr>
       <tr>
         <td class="full-width">
-          <table width="700" border="0" cellspacing="0" cellpadding="0" class="full-width" style="border-collapse:collapse; width:700px;">
+					<?php
+						if ( have_posts() ) :
+							while ( have_posts() ) :
+								the_post();
+
+								the_content(); 
+
+							endwhile;
+						endif;
+					?>
+					<!--<table width="700" border="0" cellspacing="0" cellpadding="0" class="full-width" style="border-collapse:collapse; width:700px;">
             <tr>
               <td align="left" valign="top" width="516" class="column" style="font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; font-size:14px; padding-top:10px; padding-right:15px; padding-bottom:15px; padding-left:15px; text-align:left; vertical-align:top; width:516px;">
-                <?php
-									if ( have_posts() ) :
-										while ( have_posts() ) :
-
-											the_post();
-
-											the_date( '', '<h1 style="margin:0 0 12px;">', '</h1>' );
-
-											the_content(); 
-
-										endwhile;
-									endif;
-								?>
+								content
               </td>
               <td bgcolor="#d7dadb" align="left" valign="top" width="154" class="column full-width" style="background:#d7dadb; padding:0; text-align:left; vertical-align:top; width:154px;">
                 <table width="154" border="0" cellspacing="0" cellpadding="0" class="full-width" style="border-collapse:collapse; width:154px;">
                   <tr>
                     <td bgcolor="#5e6a71" width="130" class="column" style="background:#5e6a71; border-bottom:1px solid #ffffff; color:#ffffff; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; font-size:14px; padding-top:12px; padding-right:12px; padding-bottom:20px; padding-left:12px; width: 131px;">
-                      <?php 
-												// Placeholder
-												echo '<h2 style="color:#000; font-size:16px; font-weight:normal; margin:0 0 12px; text-transform:uppercase;">Faculty</h2>';
-												echo 'News<br /><br />Grants<br /><br />Publications<br />';
-											?>
+                      <h2 style="color:#000; font-size:16px; font-weight:normal; margin:0 0 12px; text-transform:uppercase;">Faculty</h2>
+											News<br /><br />Grants<br /><br />Publications<br />
                     </td>
                   </tr>
                   <tr>
                     <td bgcolor="#8d959a" width="130" class="column" style="background:#8d959a; border-bottom:1px solid #ffffff; color:#ffffff; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; font-size:14px; padding-top:12px; padding-right:12px; padding-bottom:20px; padding-left:12px; width: 131px;">
-											<?php 
-												// Placeholder
-												echo '<h2 style="color:#000; font-size:16px; font-weight:normal; margin:0 0 12px; text-transform:uppercase;">Support</h2>';
-												echo 'Donors<br />';
-											?>
+											<h2 style="color:#000; font-size:16px; font-weight:normal; margin:0 0 12px; text-transform:uppercase;">Support</h2>
+											Donors<br />
                     </td>
                   </tr>
                   <tr>
                     <td bgcolor="#b5babe" width="130" class="column" style="background:#b5babe; border-bottom:1px solid #ffffff; color:#ffffff; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; font-size:14px; padding-top:12px; padding-right:12px; padding-bottom:20px; padding-left:12px; width: 131px;">
-                      <?php 
-												// Placeholder
-												echo '<h2 style="color:#000; font-size:16px; font-weight:normal; margin:0 0 12px; text-transform:uppercase;">Graduates</h2>';
-												echo 'May 2014<br /><br />August 2015<br />';
-											?>
+                      <h2 style="color:#000; font-size:16px; font-weight:normal; margin:0 0 12px; text-transform:uppercase;">Graduates</h2>
+											May 2014<br /><br />August 2015<br />
                     </td>
                   </tr>
                   <tr>
                     <td width="130" class="column" style="color:#5e6a71; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; font-size:14px; padding-top:12px; padding-right:12px; padding-bottom:20px; padding-left:12px; width: 131px;">
-                      <?php 
-												// Placeholder
-												echo '<h2 style="color:#000; font-size:16px; font-weight:normal; margin:0 0 12px; text-transform:uppercase;">Calendar</h2>';
-												echo 'Oct. 3 - Alumni Event<br /><br />Oct. 10 - Open House<br /><br />Oct. 31 - Trick or Treat<br />';
-											?>
+                     	<h2 style="color:#000; font-size:16px; font-weight:normal; margin:0 0 12px; text-transform:uppercase;">Calendar</h2>
+                      Oct. 3 - Alumni Event<br /><br />Oct. 10 - Open House<br /><br />Oct. 31 - Trick or Treat<br />
                     </td>
                   </tr>
                 </table>
               </td>
             </tr>
-          </table>
+          </table>-->
         </td>
       </tr>
       <tr>
@@ -77,7 +63,7 @@
           <table width="700" border="0" cellspacing="0" cellpadding="0" class="full-width" style="border-collapse:collapse; width:700px;">
             <tr>
               <td align="left" valign="top" width="516" class="column" style="color:#5e6a71; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; font-size:12px; padding-top:10px; padding-right:15px; padding-bottom:20px; padding-left:15px; text-align:left; vertical-align:top; width:516px;">
-                <img src="<?php bloginfo('stylesheet_directory'); ?>/theme-images/hd-newsletter-logo.gif" width="310" height="77" alt="WSU Department of Human Development" style="max-width:310px;" /><br />
+                <img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/hd-newsletter-logo.gif" width="310" height="77" alt="WSU Department of Human Development" style="max-width:310px;" /><br />
                 <br />
                 501 Johnson Tower<br />
                 PO Box 644852, Washington State University, Pullman WA 99164-4852<br />
@@ -86,10 +72,10 @@
               <td align="left" valign="bottom" width="136" class="column share" style="color:#5e6a71; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; font-size:12px; padding-right:12px; padding-bottom:20px; padding-left:6px; text-align:left; vertical-align:bottom; width:136px;">
                 <strong>Share this email:</strong><br />
                 <br />
-                <a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/theme-images/hd-newsletter-share-email.png" width="30" height="29" alt="Email" style="max-width:30px;" /></a>
-                <a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/theme-images/hd-newsletter-share-twitter.png" width="30" height="29" alt="Twitter" style="max-width:30px;" /></a>
-                <a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/theme-images/hd-newsletter-share-facebook.png" width="30" height="29" alt="Facebook" style="max-width:30px;" /></a>
-                <a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/theme-images/hd-newsletter-share-linkedin.png" width="30" height="29" alt="LinkedIn" style="max-width:30px;" /></a>
+                <a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/hd-newsletter-share-email.png" width="30" height="29" alt="Email" style="max-width:30px;" /></a>
+                <a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/hd-newsletter-share-twitter.png" width="30" height="29" alt="Twitter" style="max-width:30px;" /></a>
+                <a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/hd-newsletter-share-facebook.png" width="30" height="29" alt="Facebook" style="max-width:30px;" /></a>
+                <a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/hd-newsletter-share-linkedin.png" width="30" height="29" alt="LinkedIn" style="max-width:30px;" /></a>
               </td>
             </tr>
           </table>
