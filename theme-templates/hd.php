c52ce2e58763e7217b2a2b466938353cd47939e9
Phil Cable <pcable@wsu.edu>
2015-01-05 13:56:17 -0800

    Drop share buttons from HD template

--- theme-templates/hd.php
+++ theme-templates/hd.php
@@ -27,21 +27,21 @@
         <td width="700" class="full-width" style="width:700px;">
           <table width="700" border="0" cellspacing="0" cellpadding="0" class="full-width" style="border-collapse:collapse; width:700px;">
             <tr>
-              <td align="left" valign="top" width="516" class="column" style="color:#5e6a71; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; font-size:12px; padding-top:10px; padding-right:15px; padding-bottom:20px; padding-left:15px; text-align:left; vertical-align:top; width:516px;">
+              <td align="left" valign="top" width="640" class="column" style="color:#5e6a71; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; font-size:12px; padding-top:10px; padding-right:15px; padding-bottom:20px; padding-left:15px; text-align:left; vertical-align:top; width:640px;">
                 <img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/hd-newsletter-logo.gif" width="310" height="77" alt="WSU Department of Human Development" style="max-width:310px;" /><br />
                 <br />
                 501 Johnson Tower<br />
                 PO Box 644852, Washington State University, Pullman WA 99164-4852<br />
                 509-335-8439 | <a href="mailto:H_D@wsu.edu" style="color:#5e6a71; text-decoration:none;">H_D@wsu.edu</a>
               </td>
-              <td align="left" valign="bottom" width="136" class="column share" style="color:#5e6a71; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; font-size:12px; padding-right:12px; padding-bottom:20px; padding-left:6px; text-align:left; vertical-align:bottom; width:136px;">
+              <!--<td align="left" valign="bottom" width="136" class="column share" style="color:#5e6a71; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; font-size:12px; padding-right:12px; padding-bottom:20px; padding-left:6px; text-align:left; vertical-align:bottom; width:136px;">
                 <strong>Share this email:</strong><br />
                 <br />
                 <a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/hd-newsletter-share-email.png" width="30" height="29" alt="Email" style="max-width:30px;" /></a>
                 <a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/hd-newsletter-share-twitter.png" width="30" height="29" alt="Twitter" style="max-width:30px;" /></a>
                 <a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/hd-newsletter-share-facebook.png" width="30" height="29" alt="Facebook" style="max-width:30px;" /></a>
                 <a href="#"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/hd-newsletter-share-linkedin.png" width="30" height="29" alt="LinkedIn" style="max-width:30px;" /></a>
-              </td>
+              </td>-->
             </tr>
           </table>
         </td>
