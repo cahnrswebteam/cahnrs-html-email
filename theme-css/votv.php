d072648093e14c81f5f9f9b57678df7eb297fa0b
Phil Cable <pcable@wsu.edu>
2015-06-03 10:43:01 -0700

    Newsletters update

--- theme-css/votv.php
+++ theme-css/votv.php
@@ -1,165 +1,203 @@
+/* CLIENT-SPECIFIC STYLES */
+.ReadMsgBody {/* Force Hotmail to display emails at full width */
+	width: 100%;
+}
+.ExternalClass {/* Force Hotmail to display emails at full width */
+	width: 100%;
+}
+.ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {/* Force Hotmail to display normal line spacing */
+	line-height: 100%;
+}
+body, table, td, p, a, li, blockquote {/* Prevent WebKit and Windows mobile changing default text sizes */
+	-webkit-text-size-adjust: 100%; 
+	-ms-text-size-adjust: 100%;
+}
+table, td {/* Remove spacing between tables in Outlook 2007 and up */
+	mso-table-lspace: 0pt;
+	mso-table-rspace: 0pt;
+}
+img {/* Allow smoother rendering of resized image in Internet Explorer */
+	-ms-interpolation-mode: bicubic;
+}
+/* RESET STYLES */
+img {
+	border: 0;
+	height: auto;
+	line-height: 100%;
+	outline: none;
+	text-decoration: none;
+}
+table {
+	border-collapse: collapse !important;
+}
+/* LAYOUT */
 body {
 	background-color: #363637;
 	margin: 0;
 	padding: 0;
 }
-.header img {
-	display: block;
-}
-#social a {
-	color: #fff;
-	float: left;
-	width: 50%;
-	text-align: center;
-}
-#social img {
-	display: block;
-	margin: 0 auto;
-}
-#description {
-	font-size: 17px;
-	font-weight: normal;
-	text-transform: uppercase;
-}
-#issue-links a {
-	border-bottom: none;
-	color: #a3a8ac;
-	text-decoration: none;
-}
-#footer span {
-	display: block;
-}
-#global-links a {
-	color: #b5babe;
-}
-img {
-	display: block;
-	max-width: 100%;
-}
 .row {
-	font-size: 14px;
-	width: 750px;
+	width: 100% !important;
 }
 .column-1 {
 	border-right: 1px solid #b5babe;
+	padding: 0 20px 20px 0;
+	vertical-align: top;
+	width: 503px !important;
 }
-.column-1 > table {
-	width: 543px;
+.column-2 {
+	padding: 0 0 20px 20px;
+	vertical-align: top;
+	width: 166px !important;
 }
-.column-1 .image {
-	padding: 0 20px 0 20px;
+td.promo.content,
+td.promo.image {
+	padding-top: 20px;
+	text-align: left;
+}
+.column-1 .item:first-of-type > tbody > tr:first-child > td.promo.content,
+.column-1 .item:first-of-type > tbody > tr:first-child > td.promo.image {
+	padding-top: 0;
+}
+td.promo.image {
+  padding-right: 20px;
   width: 150px;
 }
-.column-1 .content {
-	padding: 0 15px 0 20px;
-}
-.column-1 .content.has-image {
-	padding: 0 15px 0 0;
-}
-.column-1 .read-more {
-	padding: 0 15px 20px 20px;
-}
-.column-1 .read-more span {
-	border-bottom: 1px solid #bec2c5;
-	display: block;
-}
-.column-1 .read-more a {
-	background: #363637;
-	color: #bec2c5;
-	display: inline-block;
-	padding: 5px 10px;
-	text-decoration: none;
-}
-.item.content_block td {
-	padding: 0 20px;
-}
-.column-2 table {
-	width: 207px;
-}
-.column-2 table td {
-	padding: 0 15px 0 15px;
-}
-h1, p {
-	margin: 0 0 1em;
-}
-h2, h3, h4, h5 {
-	margin: 0 0 0.2em;
-}
-h1 {
-	color: #5e6a71;
-	font-size: 20px;
-}
-h2,
-.column-1 h3 {
-	color: #981e32;
-	font-size: 18px;
-	font-weight: normal;
-	text-transform: uppercase;
-}
-h2 a,
-.column-1 h3 a,
-.column-2 a,
-.plugs a {
-	color: #981e32;
-	text-decoration: none;
-}
-h3 {
-	font-size: 16px;
-}
-h4 {
-	color: #981e32;
-	font-size: 15px;
+td.feed.promo.image img {
+	max-width: 150px !important;
 }
 #row-200 {
 	display: none;
 }
-@media only screen and (max-width: 480px) {
-	body {
-		width: 100% !important;
-		min-width: 100% !important;
+/* TYPE STYLES */
+.column-1 a,
+.column-2 a {
+	color: #981e32;
+	outline: none;
+	text-decoration: none;
+}
+h1 {
+	color: #555555;
+	font-size: 20px;
+	margin-top: 0;
+	margin-right: 0;
+	margin-bottom: 10px;
+	margin-left: 0;
+	text-align: left;
+}
+h2, .column-1 h3 {
+	color: #981e32;
+	font-size: 18px;
+	font-weight: normal;
+	margin-top: 0;
+	margin-right: 0;
+	margin-bottom: 10px;
+	margin-left: 0;
+	text-align: left;
+	text-transform: uppercase;
+}
+h2 a,
+.column-1 h3 a,
+.column-2 a {
+	color: #981e32;
+}
+h3 {
+	color: #555555;
+	font-size: 16px;
+	margin-top: 0;
+	margin-right: 0;
+	margin-bottom: 10px;
+	margin-left: 0;
+	text-align: left;
+}
+h4 {
+	color: #808080;
+	font-size: 14px;
+	margin-top: 0;
+	margin-right: 0;
+	margin-bottom: 10px;
+	margin-left: 0;
+	text-align: left;
+}
+p {
+	color: #5e6a71;
+	font-size: 14px;
+	line-height: 20px;
+	text-align: left;
+}
+/* MORE LINK */
+td.read-more {
+	border-bottom: 1px solid #b5babe;
+}
+td.read-more table {
+	background: #363637;
+}
+td.read-more table td {
+	padding: 5px 10px;
+}
+td.read-more a {
+	color: #b5babe;
+	font-size: 14px;
+	outline: none;
+	text-decoration: none;
+	text-transform: uppercase;
+}
+@media only screen and (max-width: 750px) {
+	#logo {
+		padding: 0 20px;
 	}
-	img {
-		height: auto !important;
-		width: 100% !important;
-	}
-	#cahnrs-logo {
-		display: block;
-		margin: 0 20px;
-	}
-	.column-1 {
-		border-right: none;
-	}
-	.full-width,
-	.column,
-	.column-1 table,
-	.column-2 table {
-		display: block;
-		height: auto !important;
-		width: 100% !important;
-	}
-	.height-fix {
-		height: auto !important;
+  #content-container {
+		width: 750px !important;
 	}
 	.shadow {
-		display: none;
+		display: none !important;
 	}
-	.column-1 .image,
-	.column-1 .content,
-	.column-1 .read-more,
-  .item.content_block td {
-		display: block;
+}
+@media only screen and (max-width: 480px) {
+	body, table, td, p, a, li, blockquote {
+		-webkit-text-size-adjust: none !important;
+	}
+	body {
+		min-width: 100% !important;
+		width: 100% !important;
+	}
+	.full-width,
+	#logo img,
+	#header,
+	#content-container {
+		display: block !important;
 		height: auto !important;
-		padding: 0 15px 0 15px !important;
-		width: auto !important;
+		width: 100% !important;
 	}
-	.column-1 .image,
-	.column-1 .read-more {
-		padding: 0 15px 20px 15px !important;
+	#header {
+		font-size: 20px !important;
+		margin-bottom: 10px;
 	}
-	.column-1 .image img {
-		max-width: 150px !important;
+  #social {
+		height: auto !important;
+		width: 100% !important;
 	}
-	.column-2 table td {
-		padding: 20px 15px 0 15px;
+	.shadow {
+		display: none !important;
+	}
+	.column-1,
+	.column-2 {
+		border: none !important;
+		display: block !important;
+		padding: 0 0 20px 0 !important;
+		width: 100% !important;
+	}
+	td.feed.promo.image,
+	td.feed.promo.content {
+		display: block;
+		width: 100%;
+	}
+	td.feed.promo.image,
+	td.feed.promo.content {
+    padding: 20px 0 0 !important;
+	}
+	td.feed.promo.content,
+	td.feed.promo.content.has-image {
+		padding: 10px 0 0 !important;
 	}
 }
--- theme-templates/votv.php
+++ theme-templates/votv.php
@@ -1,71 +1,108 @@
-<table border="0" cellpadding="0" cellspacing="0" width="100%" class="full-width" style="border-collapse:collapse; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; width:100%;">
-	<tr>
-		<td bgcolor="#363637" height="190" width="100%" class="full-width" style="background-color:#363637; height:190px; width:100%;">
-			<table align="center" border="0" cellspacing="0" cellpadding="0" width="750" class="full-width" style="width:750px;">
-				<tr>
-					<td height="190" valign="top" width="542" class="header full-width" style="color:#fff; vertical-align:top; width:542px;">
-						<span id="cahnrs-logo" style="font-size:10px;"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/vov-logo.png" width="319" height="75" alt="College of Agricultural, Human, and Natural Resource Sciences" style="height: 75px; max-width: 480px; width:319px;" /></span>
-						<img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/vov-header.png" width="542" height="115" alt="Voice of the Vine" style="font-size:24px; height:115px; width:542px;" />
-					</td>
-					<td bgcolor="#363637" height="190" valign="top" width="208" class="full-width" style="background-color:#363637; vertical-align:top; width:208px;">
-						<div style="margin: 0 20px 20px 20px;">
-							<table bgcolor="#b11730" border="0" cellspacing="0" cellpadding="0" width="100%" class="height-fix" style="background:#b11730; color:#fff; height:170px; width:100%;">
-								<tr>
-									<td align="center" id="social" style="padding:20px 20px 10px 20px; text-align:center;">
-										<a href="https://www.facebook.com/wine.wsu.edu" title="Follow us on Facebook"><img align="center" src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/newsletter-social-facebook.png" width="35" height="35" alt="Facebook" style="height:35px; max-width:35px; width:35px;" /></a>
-										<a href="http://cahnrs.wsu.edu/blog/category/voice-of-the-vine/feed" title="Subscribe to our RSS feed"><img align="center" src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/newsletter-social-rss.png" width="35" height="35" alt="RSS" style="height:35px; max-width:35px; width:35px;" /></a>
+<center>
+	<table align="center" border="0" cellpadding="0" cellspacing="0" width="100%">
+		<tr>
+			<td align="center" bgcolor="#363637">
+				<!-- BEGIN HEADER -->
+				<table align="center" bgcolor="#363637" border="0" cellpadding="0" cellspacing="0" width="750" class="full-width">
+					<tr>
+            <td align="left" valign="top" width="542" class="full-width" style="font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, san-serif;">
+							<div id="logo" style="display: block;"><img src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/votv-logo.png" width="317" height="75" alt="WSU Viticulture and Enology Program" style="color: #fff; display: block; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, san-serif; height: 75px; font-size: 9px; max-width: 317px; width: 317px;" /></div>
+							<img id="header" src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/votv-header.png" width="542" height="115" alt="Voice of the Vine" style="color: #fff; display: block; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, san-serif; font-size: 30px; height: 115px; max-width: 542px; width: 542px;" />
+						</td>
+            <td align="center" valign="top" width="208" class="full-width">
+							<!-- BEGIN SOCIAL MEDIA/TAGLINE BLOCK -->
+							<table align="center" bgcolor="#cc1232" border="0" cellspacing="0" cellpadding="20" height="170" width="168" id="social">
+  							<tr>
+									<td align="center" width="50%" style="padding-bottom: 10px; padding-right: 0;">
+										<a href="https://www.facebook.com/wine.wsu.edu" title="Follow us on Facebook" style="color: #fff;"><img align="center" src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/newsletter-social-facebook.png" width="35" height="35" alt="Follow us on Facebook" style="display: block; height: 35px; margin: 0 auto; max-width: 35px; width: 35px;" /></a>
+									</td>
+                  <td align="center" width="50%" style="padding-bottom: 10px; padding-left: 0;">
+										<a href="http://cahnrs.wsu.edu/blog/category/voice-of-the-vine/feed" title="Subscribe to our RSS feed" style="color: #fff;"><img align="center" src="<?php bloginfo( 'stylesheet_directory' ); ?>/theme-images/newsletter-social-rss.png" width="35" height="35" alt="Subscribe to our RSS feed" style="display: block; height: 35px; margin: 0 auto; max-width: 35px; width: 35px;" /></a>
 									</td>
 								</tr>
-								<tr>
-									<td align="center" valign="bottom" style="font-size:16px; line-height:20px; padding:10px 20px 20px 20px; text-align:center; text-transform:uppercase; vertical-align:bottom;">Viticulture and Enology News</td>
-								</tr>
+  							<tr>
+  							  <td align="center" colspan="2">
+                  	<span style="color: #fff; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, san-serif; font-size: 16px; line-height: 20px;">VITICULTURE AND ENOLOGY NEWS</span>
+									</td>
+							  </tr>
 							</table>
-						</div>
-					</td>
-				</tr>
-			</table>
-		</td>
-	</tr>
-	<tr>
-		<td bgcolor="#FFFFFF" width="100%" class="full-width" style="background:#fff; width:100%;">
-			<table align="center" border="0" cellspacing="0" cellpadding="0" width="756" class="full-width" style="width:756px;">
-				<tr>
-					<td class="shadow" width="1" style="background:#d2d5d7; border-left:1px solid #eff0f1; border-right:1px solid #b5babe; width:1px;"></td>
-					<td class="full-width" width="750" style="color:#5e6a71; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; width:750px;">
-						<p id="issue-links" style="color:#a3a8ac; font-size:12px; margin:10px 20px; text-transform:uppercase;"><a href="<?php the_permalink(); ?>">View Online</a> | <a href="http://cahnrs.wsu.edu/blog/category/voice-of-the-vine/">Archives</a> | <a href="*|UPDATE_PROFILE|*">Manage Subscription</a></p>
-						<?php
-							if ( have_posts() ) :
-								while ( have_posts() ) :
-									the_post();
-									the_content();
-								endwhile;
-							endif;
-						?>
-            <table class="full-width" border="0" cellspacing="0" cellpadding="0" width="750" style="width:750px;">
-              <tr>
-                <td bgcolor="#d7dadb" class="full-width plugs" width="750" style="background-color:#d7dadb; border-top:1px solid #b5babe; color:#5e6a71; font-family:'Lucida Grande','Lucida Sans Unicode',Arial,san-serif; padding: 10px 0; width:750px;">
-                	<p style="color: #5e6a71; margin: 10px 15px; padding: 0px; font-size: 12px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, san-serif;"><strong>Enjoy all three monthly newsletters from CAHNRS!</strong></p>
-									<p style="color: #5e6a71; margin: 10px 15px; padding: 0px; font-size: 12px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, san-serif;"><strong>WSU's Green Times</strong><br />
-									Organic agriculture and sustainable food systems. <a href="http://wsu.us5.list-manage2.com/subscribe?u=3cd4b2a328519c34e51f46c1d&id=114411b2ff" style="color:#981e32;"><span style="color:#981e32;">Subscribe here &raquo;</span></a></p>
-									<p style="color: #5e6a71; margin: 10px 15px; padding: 0px; font-size: 12px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, san-serif;"><strong>WSU's On Solid Ground</strong><br />
-					  			Washington agriculture and natural resources. <a href="http://wsu.us5.list-manage.com/subscribe?u=3cd4b2a328519c34e51f46c1d&id=df0b077091" style="color:#981e32;"><span style="color:#981e32;">Subscribe here &raquo;</span></a></p>
-									<p style="color: #5e6a71; margin: 10px 15px; padding: 0px; font-size: 12px; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, san-serif;"><strong>WSU's Voice of the Vine</strong><br>
-									Viticulture and enology for Washington's world-class wine industry. <a href="http://wsu.us5.list-manage.com/subscribe?u=3cd4b2a328519c34e51f46c1d&id=a3e4964764" style="color:#981e32;"><span style="color:#981e32;">Subscribe here &raquo;</span></a></p>
-			
-                </td>
-              </tr>
-            </table>
-					</td>
-					<td class="shadow" width="1" style="background:#d2d5d7; border-left:1px solid #b5babe; border-right:1px solid #eff0f1; width:1px;"></td>
-				</tr>
-			</table>
-		</td>
-	</tr>
-	<tr>
-		<td bgcolor="#363637" width="100%" class="full-width" id="footer" style="background-color:#363637; font-size:13px; padding:10px 0 20px; text-align:center; width:100%;">
-			<span style="color:#fff; margin-bottom:10px;">If you have comments or questions about a story, please <a href="mailto:erika.holmes@wsu.edu" style="color:#fff;">contact us</a>. Thank you for reading <a href="http://cahnrs.wsu.edu/blog/category/voice-of-the-vine/" style="color:#fff;">Voice of the Vine</a>.</span>
-			<span style="color:#b5babe; margin-bottom: 3px;">CAHNRS Communications, Washington State University, Pullman, WA</span>
-			<span id="global-links" style="color:#b5babe;"><a href="http://access.wsu.edu/">Accessibility</a> | <a href="http://copyright.wsu.edu/">Copyright</a> | <a href="http://policies.wsu.edu/">Policies</a></span>
-		</td>
-	</tr>
-</table>
+							<!-- END SOCIAL MEDIA/TAGLINE BLOCK -->
+						</td>
+					</tr>
+				</table>
+				<!-- END HEADER -->
+			</td>
+		</tr>
+		<tr>
+			<td align="center" bgcolor="#FFFFFF">
+				<!-- BEGIN CONTENT CONTAINER -->
+				<table align="center" width="756" border="0" cellspacing="0" cellpadding="0" class="full-width" id="content-container">
+  				<tr>
+    				<td bgcolor="#EFF0F1" width="1" class="shadow"></td>
+          	<td bgcolor="#D2D5D7" width="1" class="shadow"></td>
+          	<td bgcolor="#B5BABE" width="1" class="shadow"></td>
+    				<td align="center" width="750" class="full-width">
+							<table align="center" border="0" cellspacing="0" cellpadding="20" width="750" class="full-width">
+  							<tr>
+    							<td align="left" style="padding-bottom: 15px; padding-top: 10px; text-align: left;">
+                  	<span style="color: #a3a8ac; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, san-serif; font-size: 12px;"><a href="<?php the_permalink(); ?>" style="color: #a3a8ac; outline: none; text-decoration: none;">VIEW ONLINE</a> | <a href="http://cahnrs.wsu.edu/blog/category/voice-of-the-vine/" style="color: #a3a8ac; outline: none; text-decoration: none;">ARCHIVES</a> | <a href="*|UPDATE_PROFILE|*" style="color: #a3a8ac; outline: none; text-decoration: none;">MANAGE SUBSCRIPTION</a>
+									</td>
+  							</tr>
+								<tr>
+    							<td align="left" style="font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, san-serif; font-size: 14px; padding-bottom: 0; padding-top: 0; text-align: left;">
+										<!-- BEGIN CONTENT -->
+										<?php
+											if ( have_posts() ) :
+												while ( have_posts() ) :
+													the_post();
+													the_content();
+												endwhile;
+											endif;
+										?>
+										<!-- END CONTENT -->
+									</td>
+  							</tr>
+								<tr>
+    							<td align="left" bgcolor="#d7dadb" style="border-top: 1px solid #b5babe; text-align: left;">
+                  	<span style="color: #555; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, san-serif; font-size: 12px;"><strong>Enjoy all three monthly newsletters from CAHNRS!</strong><br />
+            				<br />
+										<strong>WSU's Green Times</strong><br />
+										Organic agriculture and sustainable food systems. <a href="http://wsu.us5.list-manage2.com/subscribe?u=3cd4b2a328519c34e51f46c1d&id=114411b2ff" style="color: #981e32; outline: none; text-decoration: none;">Subscribe here &raquo;</a><br />
+            				<br />
+										<strong>WSU's On Solid Ground</strong><br />
+					  				Washington agriculture and natural resources. <a href="http://wsu.us5.list-manage.com/subscribe?u=3cd4b2a328519c34e51f46c1d&id=df0b077091" style="color: #981e32; outline: none; text-decoration: none;">Subscribe here &raquo;</a><br />
+            				<br />
+										<strong>WSU's Voice of the Vine</strong><br />
+										Viticulture and enology for Washington's world-class wine industry. <a href="http://wsu.us5.list-manage.com/subscribe?u=3cd4b2a328519c34e51f46c1d&id=a3e4964764" style="color: #981e32; outline: none; text-decoration: none;">Subscribe here &raquo;</a></span>
+									</td>
+  							</tr>
+							</table>
+          	</td>
+    				<td bgcolor="#B5BABE" width="1" class="shadow"></td>
+          	<td bgcolor="#D2D5D7" width="1" class="shadow"></td>
+          	<td bgcolor="#EFF0F1" width="1" class="shadow"></td>
+  				</tr>
+				</table>
+				<!-- END CONTENT CONTAINER -->
+			</td>
+		</tr>
+		<tr>
+			<td align="center" bgcolor="#363637">
+				<!-- BEGIN FOOTER -->
+				<table align="center" bgcolor="#363637" border="0" cellpadding="20" cellspacing="0" width="750" class="full-width">
+					<tr>
+            <td align="center">
+            	<span style="font-family: 'Lucida Grande', 'Lucida Sans Unicode', Arial, san-serif; font-size: 12px;">
+								<span style="color: #fff;">If you have comments or questions about a story, please <a href="mailto:erika.holmes@wsu.edu" style="color: #cc1232; outline: none; text-decoration: none;">contact us</a>. Thank you for reading <a href="http://cahnrs.wsu.edu/blog/category/voice-of-the-vine/" style="color: #cc1232; outline: none; text-decoration: none;">Voice of the Vine</a>.</span><br />
+								<br />
+								<span style="color: #b5babe; font-size: 12px; line-height: 22px;">CAHNRS Communications, Washington State University, Pullman, WA<br />
+								<a href="http://access.wsu.edu/" style="color: #b5babe; outline: none; text-decoration: none;">Accessibility</a> | <a href="http://copyright.wsu.edu/" style="color: #b5babe; outline: none; text-decoration: none;">Copyright</a> | <a href="http://policies.wsu.edu/" style="color: #b5babe; outline: none; text-decoration: none;">Policies</a></span>
+							</span>
+						</td>
+					</tr>
+				</table>
+				<!-- END FOOTER -->
+			</td>
+		</tr>
+	</table>
+</center>
